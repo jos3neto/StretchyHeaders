fe6fa7443084f5cdc40f7391ac7e1ae96faf9c76
jos3barros <barros_j@yahoo.com>
2018-03-31 20:33:58 -0300

    Initial Commit

---
+++ StretchyHeaders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchyHeaders
+//
+//  Created by Jose on 31/3/18.
+//  Copyright © 2018 appcat.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
